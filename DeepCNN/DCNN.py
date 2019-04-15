9405a7019b53b21198d8abf41f7697eb9e99d474
getmykhan <mkhan7@stevens.edu>
2019-04-15 11:57:36 -0400

    Minor fixes

--- DeepCNN/DCNN.py
+++ DeepCNN/DCNN.py
@@ -1,11 +1,13 @@
 ## Importing all the Dependencies
-
+import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
 
 ## Image Preprocessing
 ## Introduce some Noise/Image Augmentation 
@@ -32,22 +34,57 @@ test = train_data_preprocess.flow_from_directory(
 
 ## Initialize the Convolutional Neural Net
 
+# Initialising the CNN
 cnn = Sequential()
 
-cnn.add(Convolution2D(32, 3, 3, input_shape = (128,128,3), activation = 'relu'))
-cnn.add(MaxPooling2D())
+# Step 1 - Convolution
+# Step 2 - Pooling
+cnn.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+cnn.add(Conv2D(32, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
 cnn.add(Flatten())
 
-## Deep Fully connected
-cnn.add(Dense(output_dim = 128, activation = 'relu'))
-cnn.add(Dense(output_dim = 128, activation = 'relu'))
-cnn.add(Dense(output_dim = 1, activation='sigmoid'))
+# Step 4 - Full connection
+cnn.add(Dense(units = 128, activation = 'relu'))
+cnn.add(Dense(units = 1, activation = 'sigmoid'))
 
-cnn.compile(optimizer = 'adam', loss='binary_crossentropy', metrics =['accuracy'])
+# Compiling the CNN
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-cnn.fit_generator(
-	train,
-	samples_per_epoch = 8000,
-	nb_epoch = 50,
-	validation_data = test,
-	nb_val_samples = 2000)
+history = cnn.fit_generator(train,
+                         steps_per_epoch = 250,
+                         epochs = 25,
+                         validation_data = test,
+                         validation_steps = 2000)
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+test_image = image.load_img('\\dataset\\single_prediction\\9. what-does-it-mean-when-cat-wags-tail.jpg', target_size=(128,128))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = cnn.predict(test_image)
+print(result)
+
+if result[0][0] == 1:
+	print('dog')
+else:
+	print('cat')
